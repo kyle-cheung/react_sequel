c0475134d78fe85375ccc01eb8971fbce18651ee
Kyle Cheung <kylecheung22@gmail.com>
2023-05-02 13:56:57 -0400

    updated bg gradient

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar"
 function App() {
   return (
     <>
-      <div className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-purple-950 to-black bg-gradient-to-r h-screen">
+      <div className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-600 to-gray-900 h-screen">
         <Navbar />
         <Query />
       </div>
